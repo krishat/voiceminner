a1ee0a609c5ccddcafc699a752d7d6f5f90032d9
Krish <akrish2011@mail.fresnostate.edu>
2013-11-19 01:15:39 -0800

    my first commit for the project my first commit for the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voiceminner::Application.config.secret_key_base = '4011f00be815901e0404720e568afc31cb3b0a3e1f13cf4a4fed179849bb265f934984d2e6dac7b5783ee60d1b51d25928dad5f9c689ab1e282520009fcc44ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Voiceminner::Application.config.session_store :cookie_store, key: '_voiceminner_session'
